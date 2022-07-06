2511950cf3ba54a5b749e81e58a8a4ea08ee4eaa
SamueleLettieri <samuele2073@gmail.com>
2022-07-06 19:12:54 +0200

    vue to do list, milestone1

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const app = new Vue(
+    {
+        el: "#root",
+        data:{
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
+    }
+)
